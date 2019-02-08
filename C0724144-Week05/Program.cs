f026ce967645b220bd3cf766dfb8d35d5ffca9c8
Sidharath Aggarwal <c0724144@mylambton.ca>
2019-02-08 18:13:52 -0500

    New commit

--- C0724144-Week05/Program.cs
+++ C0724144-Week05/Program.cs
@@ -10,6 +10,35 @@ namespace C0724144_Week05
     {
         static void Main(string[] args)
         {
+            Elevator e = new Elevator();
+            e.run();
+        }
+    }
+
+    class Node
+    {
+        public Node() { }
+        public Node floor;
+        public Node elevatorUp;
+        public string FloorNumber;
+    }
+
+    class Elevator
+    {
+        Node FirstFloor;
+        Node SecondFloor;
+        Node ThirdFloor;
+        Node FourthFloor;
+
+        public void run()
+        {
+            FirstFloor = new Node();
+            FirstFloor.FloorNumber = "First Floor";
+            Console.WriteLine("floor number is {0} ", FirstFloor.FloorNumber);
+            FirstFloor.elevatorUp = SecondFloor;
+            SecondFloor.FloorNumber = "Second Floor";
+
+            Console.WriteLine("floor number is {0} ", SecondFloor.FloorNumber);
         }
     }
 }
