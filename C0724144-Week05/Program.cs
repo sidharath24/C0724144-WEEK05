aea630929187c0d55a87efe769cdf3da4557a809
Sidharath Aggarwal <c0724144@mylambton.ca>
2019-02-08 20:35:16 -0500

    08:30 PM Commit

--- C0724144-Week05/Program.cs
+++ C0724144-Week05/Program.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace C0724144_Week05
+namespace FRI_FEB_8
 {
     class Program
     {
         static void Main(string[] args)
         {
             Elevator e = new Elevator();
-            e.run();
+            e.setup();
+            e.TraverseList();
         }
     }
 
@@ -22,33 +19,51 @@ namespace C0724144_Week05
         public Node elevatorUp;
         public string FloorNumber;
     }
+    class department
+    {
+
+    }
 
     class Elevator
     {
+        Node Head;
         Node FirstFloor;
         Node SecondFloor;
         Node ThirdFloor;
         Node FourthFloor;
 
-        public void run()
+        public void setup()
         {
             FirstFloor = new Node();
             SecondFloor = new Node();
             ThirdFloor = new Node();
             FourthFloor = new Node();
+            Head = FirstFloor;
             FirstFloor.FloorNumber = "First Floor";
             FirstFloor.elevatorUp = SecondFloor;
-            Console.WriteLine("Your current floor is {0}",FirstFloor.FloorNumber);
             SecondFloor.FloorNumber = "Second Floor";
             SecondFloor.elevatorUp = ThirdFloor;
-            Console.WriteLine("Your current floor is {0}", SecondFloor.FloorNumber);
             ThirdFloor.FloorNumber = "Third Floor";
-            ThirdFloor.elevatorUp = ThirdFloor;
-            Console.WriteLine("Your current floor is {0}", ThirdFloor.FloorNumber);
+            ThirdFloor.elevatorUp = FourthFloor;
             FourthFloor.FloorNumber = "Fourth Floor";
             FourthFloor.elevatorUp = null;
-            Console.WriteLine("Your current floor is {0}", FourthFloor.FloorNumber);
+        }
 
+        public void TraverseList()
+        {
+            Node temp;
+            temp = Head;
+
+            // where am I going to start?
+            Console.WriteLine("The first floor is " + Head.FloorNumber);
+            while (temp != null)
+            {
+                // NOW GET TO THE SECOND FLOOR!!!!
+                Console.WriteLine(temp.FloorNumber);
+                temp = temp.elevatorUp;
+                
+
+            }
 
         }
     }
